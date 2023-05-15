c4ab12aa69517e2a47e41c1029860e78bbff9cb7
vladimir <vladimir.dill@mail.ru>
2023-05-16 01:20:30 +0500

    component footer

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ShopCart from './components/shopCart/ShopCart';
+import Footer from './components/Footer/Footer';
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
         <h1>HELLO</h1>
       </div>
       <ShopCart />
+      <Footer />
     </>
   );
 }
