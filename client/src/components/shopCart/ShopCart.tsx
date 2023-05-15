7cf3f0303248b241e79b33aa7b30f0f28fe27d78
Denis <luchsound@gmail.com>
2023-05-15 17:54:51 +0300

    test

---
+++ client/src/components/shopCart/ShopCart.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './ShopCart.module.css';
+
+export default function ShopCart() {
+  return (
+    <div className={styles.Main}>
+      <h1>Cart</h1>
+    </div>
+  );
+}
