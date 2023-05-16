2bc64810eb0f16b32b9bae323e4ffe0ec704e268
frolovsan <ifseeyou@gmail.com>
2023-05-16 09:41:59 +0300

    db migrated

---
+++ server/db/migrations/20230515144325-create-color.js
@@ -0,0 +1,30 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Colors', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      color: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        defaultValue: new Date(),
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        defaultValue: new Date(),
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Colors');
+  }
+};
---
+++ server/db/models/productprops.js
@@ -0,0 +1,28 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class ProductProps extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      this.hasMany(models.Order, { foreignKey: 'orderedProduct' });
+      this.hasMany(models.Cart, { foreignKey: 'productPropsId' });
+      this.hasMany(models.Store, { foreignKey: 'productPropsId' });
+      this.belongsTo(models.Product, { foreignKey: 'productId' });
+    }
+  }
+  ProductProps.init({
+    productId: DataTypes.INTEGER,
+    colorId: DataTypes.INTEGER,
+    sizeId: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'ProductProps',
+  });
+  return ProductProps;
+};
