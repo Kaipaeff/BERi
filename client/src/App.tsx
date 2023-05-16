d73e114edbe5485234a75508176cc98d41d84cbf
vladimir <vladimir.dill@mail.ru>
2023-05-16 16:30:57 +0500

    футер 99 процентов

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import style from './app.module.css';;
+import style from './app.module.css';
 import { Navbar } from './components/Navbar/Navbar';
 import { Route, Routes } from 'react-router-dom';
 import { Clothes } from './components/Clothes/Clothes';
@@ -14,20 +14,21 @@ import AboutUs from './components/AboutUs/AboutUs';
 import PrivacyPolicy from './components/PrivacyPolicy/PrivacyPolicy';
 import TermsAndConditions from './components/TermsAndConditions/TermsAndConditions';
 import ContactUs from './components/ContactUs/ContactUs';
-import ContactUs from './components/ContactUs/ContactUs';
 
-import sliderImage from './img/images/slider/sliderImage.jpg';;
-import leftButton from './img/icons/ButtonLeft.svg';;
-import rightButton from './img/icons/ButtonRight.svg';;
+import sliderImage from './img/images/slider/sliderImage.jpg';
+import leftButton from './img/icons/ButtonLeft.svg';
+import rightButton from './img/icons/ButtonRight.svg';
+import ShippingPolicy from './components/ShippingPolicy/ShippingPolicy';
+import ReturnsAndRefunds from './components/ReturnsAndRefunds/ReturnsAndRefunds';
+import CookiesPolicy from './components/CookiesPolicy/CookiesPolicy';
+import FrequentlyAsked from './components/FrequentlyAsked/FrequentlyAsked';
+import LogIn from './components/LogIn/LogIn';
+import MyAccount from './components/MyAccount/MyAccount';
 
 function App() {
   return (
     <div className={style.wrapper}>
       <Navbar />
-      {/* <ShopCart /> */}
-
-      {/* <hr /> */}
-
       <div className={style.slider}>
         <img src={sliderImage} alt="sliderImage" />
         <img className={style.leftButton} src={leftButton} alt="leftButton" />
@@ -51,6 +52,12 @@ function App() {
           <Route path="/privacypolicy" element={<PrivacyPolicy />} />
           <Route path="/termsandconditions" element={<TermsAndConditions />} />
           <Route path="/contactus" element={<ContactUs />} />
+          <Route path="/shippingpolicy" element={<ShippingPolicy />} />
+          <Route path="/returnsandrefunds" element={<ReturnsAndRefunds />} />
+          <Route path="/cookiespolicy" element={<CookiesPolicy />} />
+          <Route path="/frequentlyasked" element={<FrequentlyAsked />} />
+          <Route path="/login" element={<LogIn />} />
+          <Route path="/myaccount" element={<MyAccount />} />
           <Route path="/cart" element={<ShopCart />} />
         </Routes>
       </div>
