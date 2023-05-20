04dcf99ff1ce9876b42d72bd1562adf297065614
Aslanbek <kaipaeff@gmail.com>
2023-05-20 23:24:21 +0300

    modal styles fixed

--- client/src/components/LogInModal/LogInModal.tsx
+++ client/src/components/LogInModal/LogInModal.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useContext, useState } from 'react';
+import React, { useContext, useState } from 'react';
 import { Context } from '../../index';
 import { observer } from 'mobx-react-lite';
 import './LogInModal.css';
