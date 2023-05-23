2ed5c4b02ce96c5f5d1b40461ab813c8b57b3232
Denis <luchsound@gmail.com>
2023-05-23 10:20:26 +0300

    pag merge

--- client/src/components/PaginationFunc/PaginationFunc.tsx
+++ client/src/components/PaginationFunc/PaginationFunc.tsx
@@ -6,17 +6,20 @@ export default function PaginationFunc() {
   const products = useAppSelector(
     (state: RootState) => state.ProductReducer.products
   );
-  
+  const copyProduct = products.map((el) => el);
+
+  const filterCopyProduct = copyProduct.filter((el) => el.rating > 4.5);
+
   const [currentPage, setCurrentPage] = useState<number>(1);
 
   const handlePageChange = (page: number) => {
     setCurrentPage(page);
   };
 
-  const productsPerPage = 5;
+  const productsPerPage = 8;
   const indexOfLastProduct = currentPage * productsPerPage;
   const indexOfFirstProduct = indexOfLastProduct - productsPerPage;
-  const currentProducts = products.slice(
+  const currentProducts = filterCopyProduct.slice(
     indexOfFirstProduct,
     indexOfLastProduct
   );
